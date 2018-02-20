f08b5e71b0fd8001534aeb566c7f9ddfeeeec64c
Eoghan Gallagher <eoghan.gallagher@gmail.com>
2018-02-20 19:17:08 +0000

    Main Menu Update

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,54 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+
+	[SerializeField]
+	public Button playerOne;
+	[SerializeField]
+	public  Button playerTwo;
+
+	void Start()
+	{
+		playerOne.Select();
+	}
+
+	public void TestButton()
+	{
+		Debug.Log( "Player One Button Clicked Ok" );
+
+		StartCoroutine( Blink( playerOne ) );
+		
+	}
+
+	public void TestButton2()
+	{
+		Debug.Log( "Player Two Button Clicked Ok" );
+
+		StartCoroutine( Blink( playerTwo ) );
+	}
+
+
+	public IEnumerator Blink( Button button )
+	{
+		int x = 0;
+
+		while( x <= 6 )
+		{
+			button.gameObject.SetActive( false );
+			yield return new WaitForSeconds( 0.15f );
+			
+			button.gameObject.SetActive( true );
+			yield return new WaitForSeconds( 0.15f );
+			
+			x++;
+		}
+
+		SceneManager.LoadScene( "level_1" );
+	}
+	
+}
---
+++ Assets/scripts/PlayerButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class PlayerButton : MonoBehaviour, ISelectHandler
+{
+
+	[SerializeField]
+	private Transform shipIcon;
+
+	[SerializeField]
+	private Transform p1Anchor , p2Anchor;
+	
+
+	public void OnSelect(BaseEventData eventData)
+    {
+        
+		if( gameObject.name == "Player1Button" )
+			shipIcon.position = p1Anchor.position;
+		
+		if( gameObject.name == "Player2Button" )
+			shipIcon.position = p2Anchor.position;
+
+    }
+	
+}
